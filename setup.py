ff90afc44ef9eb1ddadf9e3b2a31d430431e6b94
juan correa <jdcorrea1@miuandes.cl>
2023-12-16 19:13:51 -0300

    data cleaned and api

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(
+    name='EasyDolar',
+    version="0.1",
+    description="Modelo predicción tipo de cambio USD/CLP",
+    license="MIT",
+    author="EasyDolar",
+    author_email="jdcorrea1@miuandes.cl",
+    #url="https://github.com/frtm29/EasyDolar",
+    install_requires=requirements,
+    packages=find_packages(),
+    zip_safe=False
+)
