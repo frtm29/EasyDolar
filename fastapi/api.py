638c461fe2d934df7b0cd353ccd2150b93641aa9
juan correa <jdcorrea1@miuandes.cl>
2023-12-19 10:50:49 -0300

    api casi lista

--- fastapi/api.py
+++ fastapi/api.py
@@ -1,15 +1,23 @@
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from EasyDolar.archivo_python_modelo_predicción import load_model #actualizar con archivo modelo
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'Fecha demo day': "predicción CLP/USD"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-url = 'http://localhost:8000/predict'
+app.state.model = load_model() #debemos cargar el modelo entrenado como load_model()para despues dejarlo resitrado en lña app
 
 @app.get('/predict')
-def predict(email, date):
-    model.predict(email, date)
-    return {"email":"predicción"}
+def predict(email: str, date: str):
+    X_pred = pd.DataFrame(locals(), index=[0])
+    model = app.state.model #Podria faltar prepropesar los datos, haciendo alguna funciona que indique de que forma es factible pasar os datos, sino indica error
+    y_pred = model.predict(X_pred)
+    return dict(USD_CLP=float(y_pred))
